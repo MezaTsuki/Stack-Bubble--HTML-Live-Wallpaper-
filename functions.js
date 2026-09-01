926842d32d098eacbd3b06205d5f34f84b76bfa5
MesaTsuki <m8n4w4k3n@gmail.com>
2026-09-01 13:28:56 +0800

    [Update] Border and Size

--- functions.js
+++ functions.js
@@ -5,112 +5,6 @@ let body = getComputedStyle(document.body);
 
 const mainClass = document.querySelectorAll(".circle");
 
-function opacityToHex(opacity) {
-  const int = Math.round(opacity * 255);
-  return int.toString(16).padStart(2, '0');
-}
-//# __ LIVELY CONFIG __
-let pall = ["#000000", "#ffffff", "#9cecf7", "#ff9fe2"];
-let single = "#271010"
-let colorMode = 1; let opacity = 1;
-colorConfig();
-function livelyPropertyListener(name, value) {
-    switch (name) {
-
-        case"darkMode":
-            if (value) { document.body.style.background = "black"; } 
-            else { document.body.style.background = "white"; }
-            break;
-        case"shape": 
-            shapeConfig(value);
-            break;
-        case"colorMode":
-            colorMode = value; colorConfig();
-            break;
-        case"opacity":
-            opacity = value; colorConfig();
-            break;
-        case"blendMode":
-            blendConfig(value);
-            break;
-        case"singleColor":
-            single = value; colorConfig();
-            break;
-        case"palette1":
-            pall[0] = value; colorConfig(); break; 
-        case"palette2":
-            pall[1] = value; colorConfig(); break; 
-        case"palette3":
-            pall[2] = value; colorConfig(); break; 
-        case"palette4":
-            pall[3] = value; colorConfig(); break; 
-    }
-}
-
-function colorConfig() {
-    let i = -1;
-    mainClass.forEach(el => {
-        i++;
-        switch(colorMode){
-            case 1:  //! _ Palette
-                el.style.opacity = `${opacity}`;
-                el.style.background = `${pall[i]}`;
-                break;
-            case 0: //! _ Single
-                const finalOpacity = Math.max(opacity - (i * 0.25), 0);
-                el.style.opacity = `${finalOpacity}`;
-                el.style.background = `${single}`;
-                break;
-            default: break;
-        }
-    })
-}
-
-function blendConfig (mode) {
-    mainClass.forEach(el => {
-        switch(mode) {
-            case 0: //! Normal
-                el.style.mixBlendMode = "normal"; break;
-            case 1: //! Multiply
-                el.style.mixBlendMode = "multiply"; break;
-            case 2: //! Screen
-                el.style.mixBlendMode = "screen"; break;
-            case 3: //! Overlay
-                el.style.mixBlendMode = "overlay"; break;
-            case 4: //! Darken
-                el.style.mixBlendMode = "darken"; break;
-            case 5: //! Lighten
-                el.style.mixBlendMode = "lighten"; break;
-            case 6: //! Color-Dodge
-                el.style.mixBlendMode = "color-dodge"; break;
-            case 7: //! Color-Burn
-                el.style.mixBlendMode = "color-burn"; break;
-            case 8: //! Hard-Light
-                el.style.mixBlendMode = "hard-light"; break;
-            case 9: //! Soft-Light
-                el.style.mixBlendMode = "soft-light"; break;
-            case 10: //! Difference
-                el.style.mixBlendMode = "difference"; break;
-            case 11: //! Exclusion
-                el.style.mixBlendMode = "exclusion"; break;
-        }
-    })
-}
-
-function shapeConfig (shape) {
-    mainClass.forEach(el => {
-
-        switch(shape){
-            case 0:
-                el.style.borderRadius = "50%"; break;
-            case 1:
-                el.style.borderRadius = "10%"; break;
-            case 2:
-                el.style.borderRadius = "0%"; break;
-            default: break;
-        }
-    })
-}
 
 let screenHeight = body.height;
 let screenWidth = body.width;
@@ -159,7 +53,7 @@ document.addEventListener("mousemove", e => {
 
         if ((Math.abs(locatorX - mouseX) < 2) && (Math.abs(locatorY - mouseY) < 2)) {
             elapsedStop += 50;
-            if (elapsedStop >= 300) {
+            if (elapsedStop >= 500) {
                 bounce(); clearInterval(play);
                 play = null; elapsedStop = 0;
             }
---
+++ livelyPropertyListener.js
@@ -0,0 +1,144 @@
+
+function opacityToHex(opacity) {
+  const int = Math.round(opacity * 255);
+  return int.toString(16).padStart(2, '0');
+}
+
+//# __ LIVELY CONFIG __
+let pall = ["#000000", "#ffffff", "#9cecf7", "#ff9fe2"];
+let single = "#271010"
+let colorMode = 0; let fillMode = 0; let opacity = 1; 
+colorConfig();
+function livelyPropertyListener(name, value) {
+    switch (name) {
+
+        case"darkMode":
+            if (value) { document.body.style.background = "black"; } 
+            else { document.body.style.background = "white"; }
+            break;
+        case"size": 
+            sizeConfig(value);
+            break;
+        case"shape": 
+            shapeConfig(value);
+            break;
+        case"colorMode":
+            colorMode = value; colorConfig();
+            break;
+        case"fillMode":
+            fillMode = value; colorConfig();
+            break;
+        case"opacity":
+            opacity = value; colorConfig();
+            break;
+        case"blendMode":
+            blendConfig(value);
+            break;
+        case"singleColor":
+            single = value; colorConfig();
+            break;
+        case"palette1":
+            pall[0] = value; colorConfig(); break; 
+        case"palette2":
+            pall[1] = value; colorConfig(); break; 
+        case"palette3":
+            pall[2] = value; colorConfig(); break; 
+        case"palette4":
+            pall[3] = value; colorConfig(); break; 
+    }
+}
+
+function colorConfig() {
+    let objIndex = -1;
+    let i = -1;
+    mainClass.forEach(el => {
+        if (1 == 3) i = -1;
+        i++; objIndex++;
+        switch(colorMode){
+        case 1:  //! _ Palette
+
+            switch(fillMode){
+            case 0: //? __ Fill
+                el.style.opacity = `${opacity}`;
+                el.style.background = `${pall[i]}`;
+                el.style.border = ``;
+                break;
+            case 1: //? __ Border
+                el.style.opacity = `${opacity}`;
+                el.style.background = `none`;
+                el.style.border = `5px solid ${pall[i]}`;
+                break;
+            default: break;
+            }
+        break;
+
+        case 0: //! _ Single
+            const opDecay = (1 / mainClass.length);
+            const finalOpacity = Math.max(opacity - (objIndex * opDecay), 0);
+            el.style.opacity = `${finalOpacity}`;
+
+            switch(fillMode){
+            case 0: //? __ Fill
+                el.style.background = `${single}`;
+                el.style.border = ``;
+                break;
+            case 1: //? __ Border
+                el.style.background = `none`;
+                el.style.border = `5px solid ${single}`;
+            default: break;
+            }
+        break;
+        default: break;
+        }
+    })
+}
+
+function blendConfig (mode) {
+    mainClass.forEach(el => {
+        switch(mode) {
+            case 0: //! Normal
+                el.style.mixBlendMode = "normal"; break;
+            case 1: //! Multiply
+                el.style.mixBlendMode = "multiply"; break;
+            case 2: //! Screen
+                el.style.mixBlendMode = "screen"; break;
+            case 3: //! Overlay
+                el.style.mixBlendMode = "overlay"; break;
+            case 4: //! Darken
+                el.style.mixBlendMode = "darken"; break;
+            case 5: //! Lighten
+                el.style.mixBlendMode = "lighten"; break;
+            case 6: //! Color-Dodge
+                el.style.mixBlendMode = "color-dodge"; break;
+            case 7: //! Color-Burn
+                el.style.mixBlendMode = "color-burn"; break;
+            case 8: //! Hard-Light
+                el.style.mixBlendMode = "hard-light"; break;
+            case 9: //! Soft-Light
+                el.style.mixBlendMode = "soft-light"; break;
+            case 10: //! Difference
+                el.style.mixBlendMode = "difference"; break;
+            case 11: //! Exclusion
+                el.style.mixBlendMode = "exclusion"; break;
+        }
+    })
+}
+
+function sizeConfig (size) {
+    document.body.style.setProperty('--baseUnit', `${size}vw`);
+}
+
+function shapeConfig (shape) {
+    mainClass.forEach(el => {
+
+        switch(shape){
+            case 0:
+                el.style.borderRadius = "50%"; break;
+            case 1:
+                el.style.borderRadius = "10%"; break;
+            case 2:
+                el.style.borderRadius = "0%"; break;
+            default: break;
+        }
+    })
+}
